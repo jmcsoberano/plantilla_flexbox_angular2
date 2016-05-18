5989eaffc1ad2d6384eeee6cb0578ace8405b00f
José Manuel Campos Soberano <jmcsoberano@gmail.com>
2016-05-17 20:24:16 -0600

    Pagina responsiva con angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mi-app',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']    
+})
+export /**
+ * Componente principal, de la aplicación
+ */
+class AppComponent {
+    titulo = "Plantilla Stack Tokens";
+}
+
